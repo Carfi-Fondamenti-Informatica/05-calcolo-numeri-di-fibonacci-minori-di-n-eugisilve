4603641873fb20f3f86746152aeca12bac18cd00
eugisilve <114446323+eugisilve@users.noreply.github.com>
2022-11-28 08:42:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int  a=1, b=1, c=0, n=0;
+    cin >> n;
+    cout << a << endl << b << endl;
+    for ( int i =2; i <=n ; i++ ) {
+        c = a+b ;
+        a=b;
+        b=c;
+        if (c<=n)
+        cout << c << endl;
+    }
+    return 0;
 }
